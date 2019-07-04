6277feb08ec3a1b40bb2e064f7c66c9c0d8d3d70
zheli1x <zhex.h.li@intel.com>
2019-07-04 12:46:43 +0800

    Enable test_float, test_setjmp, AES-XTS test cases
    The relevant features have been added in OP-TEE OS,
    so enable these test cases in xtest
    
    Change-Id: I13dc8c920f87f33220c5d2948dc6ce1083209a8b
    Signed-off-by: zheli1x <zhex.h.li@intel.com>

--- ta/os_test/os_test.c
+++ ta/os_test/os_test.c
@@ -576,8 +576,6 @@ static TEE_Result test_time(void)
 	return TEE_SUCCESS;
 }
 
-/* TODO: will enable float test for x86 */
-#if 0
 #ifdef CFG_TA_FLOAT_SUPPORT
 static bool my_dcmpeq(double v1, double v2, double prec)
 {
@@ -661,10 +659,7 @@ static TEE_Result test_float(void)
 	return TEE_SUCCESS;
 }
 #endif /*CFG_TA_FLOAT_SUPPORT*/
-#endif
 
-/* TODO: will enable jmp test for x86 */
-#if 0
 static __noinline void call_longjmp(jmp_buf env)
 {
 	DMSG("Calling longjmp");
@@ -684,7 +679,6 @@ static TEE_Result test_setjmp(void)
 		return TEE_ERROR_GENERIC;
 	}
 }
-#endif
 
 TEE_Result ta_entry_basic(uint32_t param_types, TEE_Param params[4])
 {
@@ -708,14 +702,13 @@ TEE_Result ta_entry_basic(uint32_t param_types, TEE_Param params[4])
 	if (res != TEE_SUCCESS)
 		return res;
 
-	/* TODO: will enable these when support is available */
-	/*res = test_float();
+	res = test_float();
 	if (res != TEE_SUCCESS)
 		return res;
 
 	res = test_setjmp();
 	if (res != TEE_SUCCESS)
-		return res;*/
+		return res;
 
 	return TEE_SUCCESS;
 }
