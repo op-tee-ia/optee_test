0706240d823421c06f05bceedcccc83ebd51c649
Shirley Zhao <shirley.zhao@intel.com>
2022-01-06 13:40:50 +0800

    Align with ARM, do not seperate PT_LOAD segment.
    Signed-off-by: Shirley Zhao <shirley.zhao@intel.com>

--- ta/os_test_lib/sub.mk
+++ ta/os_test_lib/sub.mk
@@ -1,6 +1,9 @@
 cppflags-$(WITH_TLS_TESTS) += -DWITH_TLS_TESTS=1
 global-incdirs-y += include
 srcs-y += os_test_lib.c
+#ifdef X86_64
+LDFLAGS += -z noseparate-code
+#endif
 ifneq ($(COMPILER),clang)
 srcs-y += os_test_lib_cxx.cpp
 endif
--- ta/os_test_lib_dl/sub.mk
+++ ta/os_test_lib_dl/sub.mk
@@ -1,5 +1,8 @@
 global-incdirs-y += include
 srcs-y += os_test_lib_dl.c
+#ifdef X86_64
+LDFLAGS += -z noseparate-code
+#endif
 ifneq ($(COMPILER),clang)
 srcs-y += os_test_lib_dl_cxx.cpp
 endif
